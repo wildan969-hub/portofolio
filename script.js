836b2702abc769e4e9d395e59a8c9bccd8e2c1df
wildanhilmi-hub <mubarokwildan329@gmail.com>
2026-07-14 23:17:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+const darkBtn = document.getElementById("darkBtn");
+
+
+// Hamburger Menu
+
+menuBtn.onclick = function(){
+    navMenu.classList.toggle("active");
+};
+
+
+// Dark Mode
+
+darkBtn.onclick = function(){
+    document.body.classList.toggle("dark");
+};
+
+
+// Tutup menu setelah klik link
+
+const menuLink = document.querySelectorAll(".nav-menu a");
+
+menuLink.forEach(link => {
+
+    link.onclick = function(){
+        navMenu.classList.remove("active");
+    }
+
+});
+
+
+// Animasi scroll
+
+const sections = document.querySelectorAll("section");
+
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+
+        }
+
+    });
+
+});
+
+
+sections.forEach(section => {
+
+    section.style.opacity = "0";
+    section.style.transform = "translateY(40px)";
+    section.style.transition = "0.8s";
+
+    observer.observe(section);
+
+});
